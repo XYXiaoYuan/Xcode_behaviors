34b689b62925b234ea060c29d4ca35e0627b05be
yuan <664995775@qq.com>
2022-11-03 22:52:24 +0800

    update sh

--- flutter_pplive.sh
+++ flutter_pplive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 osascript <<END
-tell application "iTerm"
+tell application "Terminal"
 if not (exists window 1) then reopen
 activate
-do script "cd `pwd`;cd pplive-flutter;flutter pub get;cd ../;pod update" in window 1
+do script "cd `pwd`; cd pplive-flutter; fvm flutter clean; fvm use 2.5.2; fvm flutter pub get;cd ../; pod install" in window 1
 end tell
 END
